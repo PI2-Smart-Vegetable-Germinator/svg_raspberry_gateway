0c028fa356162c2b2a85819bc117a6ebbbbc87fd
Felipe Hargreaves <fhargreaves00@gmail.com>
2019-10-30 12:05:47 -0300

    Begin cron logic to fetch sensor data

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,4 @@
+import json
 import os
 import unittest
 
@@ -5,6 +6,9 @@ from flask import Flask
 from flask import render_template, jsonify
 from flask_cors import CORS
 from flask_socketio import SocketIO
+
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
 
 from dotenv import load_dotenv
 
@@ -25,6 +29,19 @@ from project.api.images.views import image_blueprint
 from project.interface.views import interface_blueprint
 app.register_blueprint(image_blueprint)
 app.register_blueprint(interface_blueprint)
+
+def get_updated_info():
+    with open(os.path.dirname(__file__) + '../fake_data.json') as json_file:
+        sensor_info = json.load(json_file)
+        data = {
+            'currentTemperature': sensor_info['temperature'],
+            'currentHumidity': sensor_info['humidity'],
+        }
+
+
+cron = BackgroundScheduler()
+
+
 
 @app.route('/test')
 def test():

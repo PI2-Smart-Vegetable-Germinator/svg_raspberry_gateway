8dcbe25304e0755e28235e34f81a8472d491ef58
Felipe Hargreaves <fhargreaves00@gmail.com>
2019-11-05 22:10:31 -0300

    Add seedling selection before starting a planting and integrate planting actions with backend.

--- project/interface/views.py
+++ project/interface/views.py
@@ -48,7 +48,10 @@ def home():
         machine_info = json.load(json_file)
 
         if not machine_info.get('plantingActive'):
-            return render_template('planting.html')
+            seedlings_file = open(os.path.dirname(__file__) + '/../../assets/seedlings.json')
+            seedlings_data = json.load(seedlings_file)
+            seedlings_file.close()
+            return render_template('planting.html', seedlings=seedlings_data['seedlings'])
     return render_template('home.html')
 
 @interface_blueprint.route('/api/confirm_pairing')
@@ -79,3 +82,27 @@ def end_irrigation():
     response = requests.post('%s/api/end_irrigation' % os.getenv('EXTERNAL_GATEWAY_URL'), json=data)
 
     return jsonify({'success': True}), 200
+
+@interface_blueprint.route('/api/start_planting', methods=['POST'])
+def start_planting():
+    post_data = request.get_json()
+    with open(os.path.dirname(__file__) + '/../../assets/machine_info.json') as json_file:
+        machine_info = json.load(json_file)
+
+        post_data['machineId'] = machine_info.get('id')
+
+    with open(os.path.dirname(__file__) + '/../../test_data.json') as json_file:
+        sensor_info = json.load(json_file)
+        post_data['currentTemperature'] =  sensor_info['temperature']
+        post_data['currentHumidity'] = sensor_info['humidity']
+
+    response = requests.post('%s/api/start_planting' % os.getenv('EXTERNAL_GATEWAY_URL'), json=post_data)
+
+    response_json = response.json()
+    machine_info['plantingActive'] = True
+    machine_info['plantingId'] = response_json.get('plantingId')
+
+    with open(os.path.dirname(__file__) + '/../../assets/machine_info.json', 'w') as json_file:
+        json.dump(machine_info, json_file)
+
+    return jsonify({'success': True}), 201

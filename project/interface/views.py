94a81837988c32b1c9c226d34d43ee284ba02621
Mariana Picolo <marianapicolo@hotmail.com>
2019-11-11 01:48:11 -0300

    Add irrigation endpoints for the app

--- project/interface/views.py
+++ project/interface/views.py
@@ -3,13 +3,16 @@ from flask import jsonify
 from flask import render_template
 from flask import request
 
-import requests, os, json
+import requests
+import os
+import json
 
 from project import socketio
 
 from random import randrange
 
 interface_blueprint = Blueprint("interface", __name__)
+
 
 @interface_blueprint.route('/app/start')
 def start_system():
@@ -21,43 +24,52 @@ def start_system():
             machine_id = machine_info.get('id')
     return render_template('start_screen.html', hasId=(machine_id != None))
 
+
 @interface_blueprint.route('/app/pairing')
 def pair_device():
     pin = randrange(9999)
-    data = {'pincode' : ('%04d' % pin)}
+    data = {'pincode': ('%04d' % pin)}
+    plantingActive = None
 
     with open(os.path.dirname(__file__) + '/../../assets/machine_info.json') as json_file:
         machine_info = json.load(json_file)
+        plantingActive = machine_info.get('plantingActive')
 
         if machine_info.get('id'):
             data['id'] = machine_info.get('id')
 
-    response = requests.post('%s/api/machine' % os.getenv('EXTERNAL_GATEWAY_URL'), json=data)
+    response = requests.post('%s/api/machine' %
+                             os.getenv('EXTERNAL_GATEWAY_URL'), json=data)
 
     if response.json().get('machineId'):
         with open(os.path.dirname(__file__) + '/../../assets/machine_info.json', 'w') as json_file:
-            existing_info = json.load(json_file)
+            existing_info = {}
+            existing_info['plantingActive'] = plantingActive
             existing_info['id'] = response.json()['machineId']
             json.dump(existing_info, json_file)
 
     return render_template('pairing.html', pin=('%04d' % pin))
 
+
 @interface_blueprint.route('/app/home')
 def home():
-    with open(os.path.dirname(__file__) + '/../../assets/machine_info.json') as json_file:
+    with open(os.path.dirname(__file__) + '/../../assets/machine_info.json', 'r+') as json_file:
         machine_info = json.load(json_file)
 
         if not machine_info.get('plantingActive'):
-            seedlings_file = open(os.path.dirname(__file__) + '/../../assets/seedlings.json')
+            seedlings_file = open(os.path.dirname(
+                __file__) + '/../../assets/seedlings.json')
             seedlings_data = json.load(seedlings_file)
             seedlings_file.close()
             return render_template('planting.html', seedlings=seedlings_data['seedlings'])
     return render_template('home.html')
 
+
 @interface_blueprint.route('/api/confirm_pairing')
 def confirm_pairing():
     socketio.emit('device_paired', {'success': True})
     return jsonify({'success': True}), 200
+
 
 @interface_blueprint.route('/api/start_irrigation')
 def start_irrigation():
@@ -67,9 +79,22 @@ def start_irrigation():
 
         data['plantingId'] = machine_info.get('plantingId')
 
-    response = requests.post('%s/api/start_irrigation' % os.getenv('EXTERNAL_GATEWAY_URL'), json=data)
+    response = requests.post('%s/api/start_irrigation' %
+                            os.getenv('EXTERNAL_GATEWAY_URL'), json=data)
 
     return jsonify({'success': True}), 200
+
+
+@interface_blueprint.route('/api/app/start_irrigation')
+def app_start_irrigation():
+
+    return jsonify({'success': True, 'app_start_irrigation': 'testinho-top'}), 200
+
+@interface_blueprint.route('/api/app/end_irrigation')
+def app_end_irrigation():
+
+    return jsonify({'success': True, 'app_end_irrigation': 'testinho-fim'}), 200
+
 
 @interface_blueprint.route('/api/end_irrigation')
 def end_irrigation():
@@ -79,9 +104,11 @@ def end_irrigation():
 
         data['plantingId'] = machine_info.get('plantingId')
 
-    response = requests.post('%s/api/end_irrigation' % os.getenv('EXTERNAL_GATEWAY_URL'), json=data)
+    response = requests.post('%s/api/end_irrigation' %
+                             os.getenv('EXTERNAL_GATEWAY_URL'), json=data)
 
     return jsonify({'success': True}), 200
+
 
 @interface_blueprint.route('/api/start_planting', methods=['POST'])
 def start_planting():
@@ -93,10 +120,11 @@ def start_planting():
 
     with open(os.path.dirname(__file__) + '/../../test_data.json') as json_file:
         sensor_info = json.load(json_file)
-        post_data['currentTemperature'] =  sensor_info['temperature']
+        post_data['currentTemperature'] = sensor_info['temperature']
         post_data['currentHumidity'] = sensor_info['humidity']
 
-    response = requests.post('%s/api/start_planting' % os.getenv('EXTERNAL_GATEWAY_URL'), json=post_data)
+    response = requests.post('%s/api/start_planting' %
+                             os.getenv('EXTERNAL_GATEWAY_URL'), json=post_data)
 
     response_json = response.json()
     machine_info['plantingActive'] = True
@@ -107,6 +135,7 @@ def start_planting():
 
     return jsonify({'success': True}), 201
 
+
 @interface_blueprint.route('/api/end_planting')
 def end_planting():
     post_data = {}
@@ -115,7 +144,8 @@ def end_planting():
 
         post_data['plantingId'] = machine_info.get('plantingId')
 
-    response = requests.post('%s/api/end_planting' % os.getenv('EXTERNAL_GATEWAY_URL'), json=post_data)
+    response = requests.post('%s/api/end_planting' %
+                             os.getenv('EXTERNAL_GATEWAY_URL'), json=post_data)
     machine_info['plantingActive'] = False
     machine_info['plantingId'] = None
 
